9a7d2f02fda379cf1fda6aa464975a846cbf5ffa
IamTiti <titibamiduro@gmail.com>
2022-07-26 10:51:01 -0700

    w/o

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+/**
+ * main - Print alphabet w/o two letters
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char ch = 'a';
+
+	while (ch <= 'z')
+	{
+		if (ch != 'q' && ch != 'e')
+		{
+			putchar(ch);
+		}
+		ch++;
+	}
+	putchar('\n');
+	return (0);
+}
