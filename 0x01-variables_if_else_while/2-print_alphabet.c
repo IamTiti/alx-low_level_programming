28f965c5bd8ee85d2a825dd9282ae52f75b7c0f9
IamTiti <titibamiduro@gmail.com>
2022-07-26 10:24:52 -0700

    lcase

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+/**
+ * main - Pritn alphabet
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char ch = 'a';
+
+	while (ch <= 'z')
+	{
+		putchar(z);
+		z++;
+	}
+	putchar('\n');
+	return (0);
