b04a7779bd1045ecaae1b18924db00b0f18104d1
IamTiti <titibamiduro@gmail.com>
2022-07-26 11:09:47 -0700

    num

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - print numbers 0-9
+ * Return: 0
+ */
+int main(void)
+{
+	int i = 0;
+
+	while (i <= 9)
+	{
+	printf("%d, n);
+	n++
+	}
+	printf("\n");
+	return (0);
+}
