22a35c89c76601a70543a6e51cb57be4b0d97ee9
IamTiti <titibamiduro@gmail.com>
2022-07-27 13:24:43 -0700

    single digit

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main- print all possible combinators of single digit
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int ch = '0';
+
+	while (ch <= '9')
+	{
+	putchar(ch);
+	if (ch < '9')
+	{
+	putchar(',');
+	putchar(' ');
+	}
+	ch++;
+	putchar ('\n');
+
+	return (0);
+}
