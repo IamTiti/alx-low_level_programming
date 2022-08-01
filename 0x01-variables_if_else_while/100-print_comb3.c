f2f6fe648105dbc024df5b709492b0f89ed9b61f
IamTiti <titibamiduro@gmail.com>
2022-08-01 02:52:06 -0700

    2-digit

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - print all 2 digit combinations
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int i, j;
+
+	for (i = 0; i < 9; i++)
+	{
+	for (j = i + 1; j < 10; j++)
+	{
+	putchar((i % 10) + '0');
+	putchar((j % 10) + '0');
+	if (i == 8 && j == 9)
+	continue;
+	putchar(',');
+	putchar(' ');
+	}
+	}
+	putchar ('\n');
+	return (0);
+}
