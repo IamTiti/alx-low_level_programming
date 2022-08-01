3001861b7d42e1f754ebb7a966252473e2bc9112
IamTiti <titibamiduro@gmail.com>
2022-08-01 03:05:14 -0700

    3 digit

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ * main - print all 3 digit combinations
+ *
+ * Return: 0
+ */
+int main(void)
+{
+        int i, j;
+
+        for (i = 0; i < 9; i++)
+        {
+        for (j = i + 1; j < 10; j++)
+        {
+	for (k = i + 1; k < 10; k++)
+        putchar((i % 10) + '0');
+        putchar((j % 10) + '0');
+	putchar((k % 10) + '0');
+        if (i == 8 && j == 9 && k == 9)
+        continue;
+        putchar(',');
+        putchar(' ');
+        }
+        }
+        putchar ('\n');
+        return (0);
+}
+
