733308f86f0ccd8b25b726dc1c66289351e99435
IamTiti <titibamiduro@gmail.com>
2022-08-01 03:49:07 -0700

    output update

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -6,15 +6,15 @@
  */
 int main(void)
 {
-        int i, j, k;
+	int i, j, k;
 
-        for (i = 0; i < 9; i++)
-        {
-        for (j = i + 1; j < 10; j++)
-        {
+	for (i = 0; i < 8; i++)
+	{
+	for (j = i + 1; j < 9; j++)
+	{
 	for (k = i + 1; k < 10; k++)
-        putchar((i % 10) + '0');
-        putchar((j % 10) + '0');
+	putchar((i % 10) + '0');
+	putchar((j % 10) + '0');
 	putchar((k % 10) + '0');
 	if (i == 7 && j == 8 && k == 9)
 	continue;
