72ee21918bfa0a19de12dcb27ce1b2b873346782
IamTiti <titibamiduro@gmail.com>
2022-08-06 00:58:37 -0700

    add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * add - add two integers
+ *@n1: first integer to be computed
+ *@n2: second integer to be computed
+ *
+ * Return: integer value
+ */
+int add(int n1, int n2)
+{
+	int n1;
+	int n2;
+
+	for (n1 = 0; n1++)
+	{
+	for (n2 = 0; n2++)
+		{
+		_putchar(n1 + n2);
+		}
+		putchar("\n");
+	}
+	return (n1 + n2);
+}
