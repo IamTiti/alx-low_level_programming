a0935e161ed9f40d09cdb01727b78104f7035525
IamTiti <titibamiduro@gmail.com>
2022-08-10 01:05:17 -0700

    multiples

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,21 @@
+#include "stdio.h"
+/**
+ * main - a program that computes and prints
+ * sum of all integers of 3 or 5 below 1024
+ * i: integer
+ * r: result
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int x, sum = 0;
+
+	for (x = 0; x < 1024; x++)
+	{
+		if ((x % 3) == 0 || (x % 5) == 0)
+			sum = sum + x
+	}
+	printf("%d\n", sum);
+	return (0);
+}
