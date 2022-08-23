a83286c8d791d462e3c376ffd0c9cb4def6b93e6
IamTiti <titibamiduro@gmail.com>
2022-08-23 03:14:28 -0700

    file 3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_numbers -  checks for a digit (0 through 9).
+ *
+ * Return: Always 0.
+ */
+void print_numbers(void)
+{
+	int x;
+
+	for (x = 48; x < 58; x++)
+	{
+		_putchar(x);
+	}
+	_putchar('\n');
+}
